e37f221a857183b94597c51f2ac9c4bbf89ee886
Trần Hoàng <trandanghoang2811@gmail.com>
2023-12-23 22:46:02 +0700

    FH-40 | Refactor code

--- src/controllers/staff/accommodation.controller.ts
+++ src/controllers/staff/accommodation.controller.ts
@@ -1,24 +1,6 @@
 import {service} from '@loopback/core';
-import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
-} from '@loopback/repository';
-import {
-  api,
-  del,
-  get,
-  getModelSchemaRef,
-  param,
-  patch,
-  post,
-  put,
-  requestBody,
-  response,
-} from '@loopback/rest';
+import {Filter, repository} from '@loopback/repository';
+import {api, get, getModelSchemaRef, param, response} from '@loopback/rest';
 import {EUserRole} from '../../enums/user';
 import {IPaginationList} from '../../interfaces/common';
 import {Accommodation, AccommodationRatingRelations} from '../../models';
@@ -35,38 +17,6 @@ export class AccommodationController {
     public accommodationService: AccommodationService,
   ) {}
 
-  @post('/accommodations')
-  @response(200, {
-    description: 'Accommodation model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Accommodation)}},
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Accommodation, {
-            title: 'NewAccommodation',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    accommodation: Omit<Accommodation, 'id'>,
-  ): Promise<Accommodation> {
-    return this.accommodationRepository.create(accommodation);
-  }
-
-  @get('/accommodations/count')
-  @response(200, {
-    description: 'Accommodation model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(Accommodation) where?: Where<Accommodation>,
-  ): Promise<Count> {
-    return this.accommodationRepository.count(where);
-  }
-
   @get('/accommodations')
   @response(200, {
     description: 'Paginate accommodation list',
@@ -82,25 +32,6 @@ export class AccommodationController {
     return this.accommodationService.paginate(filter);
   }
 
-  @patch('/accommodations')
-  @response(200, {
-    description: 'Accommodation PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Accommodation, {partial: true}),
-        },
-      },
-    })
-    accommodation: Accommodation,
-    @param.where(Accommodation) where?: Where<Accommodation>,
-  ): Promise<Count> {
-    return this.accommodationRepository.updateAll(accommodation, where);
-  }
-
   @get('/accommodations/{id}')
   @response(200, {
     description: 'Accommodation model instance',
@@ -114,43 +45,5 @@ export class AccommodationController {
     @param.path.string('id') id: string,
   ): Promise<AccommodationRatingRelations | null> {
     return this.accommodationService.getById(id);
-  }
-
-
-  @patch('/accommodations/{id}')
-  @response(204, {
-    description: 'Accommodation PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Accommodation, {partial: true}),
-        },
-      },
-    })
-    accommodation: Accommodation,
-  ): Promise<void> {
-    await this.accommodationRepository.updateById(id, accommodation);
-  }
-
-  @put('/accommodations/{id}')
-  @response(204, {
-    description: 'Accommodation PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() accommodation: Accommodation,
-  ): Promise<void> {
-    await this.accommodationRepository.replaceById(id, accommodation);
-  }
-
-  @del('/accommodations/{id}')
-  @response(204, {
-    description: 'Accommodation DELETE success',
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.accommodationRepository.deleteById(id);
   }
 }
